1e1ffac79b34e791a75627677aa7c252cb98b9b0
Cristian <cri.jimenez24@gmail.com>
2024-03-28 22:24:09 -0300

    Implementacion video dinamico y setInicio del mismo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
-import { Reproductor } from './reproductor.module.js';
-
-const musica = document.querySelector("#musica");
-const peliculas = document.querySelector("#peliculas");
-const series = document.querySelector("#series");
+const musicaId = document.querySelector("#musica");
+const peliculasId = document.querySelector("#peliculas");
+const seriesId = document.querySelector("#series");
 
 const iife = (() => {
     function functionPrivada (id, url) {
@@ -16,6 +14,54 @@ const iife = (() => {
     }
 })();
 
-iife.functionPublica(musica, 'https://www.youtube.com/watch?v=1ddK89KqVe8');
-iife.functionPublica(peliculas, 'https://www.youtube.com/watch?v=d3XN49jmmGM');
-iife.functionPublica(series, 'https://www.youtube.com/watch?v=wtA1pqbHTeM');
+
+class Multimedia {
+
+    #url;
+
+    constructor(url){
+        this.#url = url;
+    }
+
+    get getUrl(){
+        return this.#url;
+    }
+
+    setInicio(){
+        const url = this.getUrl + ';start=' + 0;
+        iife.functionPublica(this.id, url);
+    }
+
+}
+
+class Reproductor extends Multimedia {
+
+    id;
+
+    constructor(url, id){
+        super(url);
+        this.id = id;
+    }
+
+    playMultimedia(){
+        iife.functionPublica(this.id, this.getUrl);
+    }
+
+    setInicio(segundos){
+        const url = this.getUrl + ';start=' + segundos;
+        console.log(url)
+        iife.functionPublica(this.id, url);
+    }
+}
+
+const musica = new Reproductor("https://www.youtube.com/embed/ngRPQKRJ89I?si=8pY4QwYxLDHAXAMI&amp",musicaId);
+const peliculas = new Reproductor("https://www.youtube.com/embed/kuaMCweiZi0?si=21d23kmt2m653WT7", peliculasId);
+const series = new Reproductor("https://www.youtube.com/embed/PfID_33TL_A?si=-irqZnqrdQLP1nij&amp" ,seriesId);
+
+musica.playMultimedia();
+musica.setInicio(44);
+peliculas.playMultimedia();
+series.playMultimedia();
+series.setInicio(12);
+
+
